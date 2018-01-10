ec80c374cb7af83cea66bf07de05dfa810abff55
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2018-01-10 17:04:51 +0900

    second commit

--- TwinMetric.cpp
+++ TwinMetric.cpp
@@ -1,14 +1,15 @@
+// This file is public domain software (PDS).
 #include <ft2build.h>
-
 #include FT_FREETYPE_H
 
 #include <windows.h>
 #include <stdio.h>
+#include <assert.h>
 
-#define FONT_NAME "DejaVu Sans"
-#define FONT_FILE "DejaVuSans.ttf"
+#define FONT_NAME "MS Gothic"
+#define FONT_FILE "msgothic.ttf"
 
-SIZE TestWin(INT nHeight)
+SIZE TestWin(const char *text, INT nPointSize)
 {
     SIZE siz = { 0, 0 };
     LOGFONT lf;
@@ -16,22 +17,23 @@ SIZE TestWin(INT nHeight)
     HDC hDC = CreateCompatibleDC(NULL);
     {
         ZeroMemory(&lf, sizeof(lf));
-        lf.lfHeight = -MulDiv(nHeight, GetDeviceCaps(hDC, LOGPIXELSY), 72);
+        lf.lfHeight = -MulDiv(nPointSize, GetDeviceCaps(hDC, LOGPIXELSY), 72);
         lstrcpy(lf.lfFaceName, TEXT(FONT_NAME));
         HFONT hFont = CreateFontIndirect(&lf);
 
         HGDIOBJ hFontOld = SelectObject(hDC, hFont);
         {
-            GetTextExtentPoint32A(hDC, "W", 1, &siz);
+            GetTextExtentPoint32A(hDC, text, lstrlenA(text), &siz);
         }
         SelectObject(hDC, hFontOld);
+        DeleteObject(hFont);
     }
     DeleteDC(hDC);
 
     return siz;
 }
 
-SIZE TestFT(INT nHeight)
+SIZE TestFT(const char *text, INT nPointSize)
 {
     SIZE siz = { 0, 0 };
     FT_Library library;
@@ -45,21 +47,7 @@ SIZE TestFT(INT nHeight)
     FT_Face face;
     FT_New_Face(library, szPath, 0, &face);
 
-    FT_Size_RequestRec req;
-    req.type = FT_SIZE_REQUEST_TYPE_NOMINAL;
-    req.width = 0;
-    req.height = nHeight;
-    req.horiResolution = 96;
-    req.vertResolution = 96;
-    FT_Request_Size(face, &req);
-
-    FT_Load_Char(face, 'W', FT_LOAD_RENDER);
-    FT_GlyphSlot slot = face->glyph;
-
-    INT cx = slot->advance.x;
-    INT cy = slot->metrics.height;
-    siz.cx = cx;
-    siz.cy = cy * 96 / 72;
+    ...(1)...
 
     FT_Done_Face(face);
     FT_Done_FreeType(library);
@@ -68,9 +56,16 @@ SIZE TestFT(INT nHeight)
 
 int main(void)
 {
-    SIZE sizWin = TestWin(100);
-    SIZE sizFT = TestFT(100);
+    const char *text = "This is a sample text.";
+
+    SIZE sizWin = TestWin(text, 200);
     printf("sizWin: %ld, %ld\n", sizWin.cx, sizWin.cy);
+
+    SIZE sizFT = TestFT(text, 200);
     printf("sizFT: %ld, %ld\n", sizFT.cx, sizFT.cy);
+
+    assert(sizWin.cx == sizFT.cx);
+    assert(sizWin.cy == sizFT.cy);
+
     return 0;
 }
